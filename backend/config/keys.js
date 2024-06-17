c81e930607f60e70083952ff17b741a46463ddce
Dhawal Chheda <dhawalc@users.noreply.github.com>
2024-06-16 23:15:36 -0700

    Update keys.js

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    googleClientID: '31573058721-22flvb11g329cljlpp5odp6reoajmqd7.apps.googleusercontent.com',
-    googleClientSecret: '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'
+    googleClientID: 'your client id',
+    googleClientSecret: 'your client secret'
   };
   
